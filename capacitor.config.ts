c83759e9607f3adde377914d26aaa6ba1a47452d
Sanjana <sanjana20.hlt@gmail.com>
2024-12-04 14:17:55 +0530

    angular not working

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'myangularapp.com',
+  appName: 'my-angular-app',
+  webDir: 'dist'
+};
+
+export default config;
